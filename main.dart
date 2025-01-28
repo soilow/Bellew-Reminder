ba1531175ccbc04a67c7d599e8f0330b8299aeab
Dmitry Zmerzlov <zmerzlovd@gmail.com>
2025-01-28 22:29:33 +0300

    Init

--- main.dart
+++ main.dart
@@ -2,15 +2,14 @@ import 'dart:async';
 
 import 'package:teledart/model.dart';
 import 'package:teledart/teledart.dart';
-import 'package:teledart/telgram.dart';
+import 'package:teledart/telegram.dart';
 import 'dart:io' as io;
 
-const String apiKey = Platgrom.environment['API_TELEGRAM_KEY'] ?? '';
+const String apiKey = Platform.environment['API_TELEGRAM_KEY'] ?? '';
 
 Future<void> main() async {
-	final telegram  = Telegram(apiKey);
-	final event = Event((await telegram.getMe()).username!);
-	final teledart = TeleDart(telegram, event);
+	final username = (await Telegram(apiKey).getMe().username);
+	final teledart = TeleDart(apiKey, Event(username!));
 
 	teledart.start();
 
